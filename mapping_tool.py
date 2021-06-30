a52e76e888b95c527c041a3b1dfe42a8cb785566
Pyder3 <73122408+Pyder3@users.noreply.github.com>
2021-06-30 10:03:14 +0530

    Create mapping_tool.py
    It somewhat eases the process of mapping coordinated in the code wrt your display.

---
+++ mapping_tool.py
@@ -0,0 +1,38 @@
+import pyautogui as pyg
+import time
+
+print("Welcome to mapping tool for zoom bot!!)
+print("This is a one time process for your program until you move your zoom client window around, so don't do that!!")
+      
+print("Position you mouse on the join button in the Zoom Client in under 7 seconds!!!!")
+time.sleep(7)
+x = pyg.position()[0]
+y = pyg.position()[1]
+print(f"Copt paste the follwing in place of x and y coordinates under the join_meeting_ID function in  the click on join meeting command x={x}, y={y}")
+print("The program will wait for you for 69 seconds!!!")
+time.sleep(69)
+      
+print("Position you mouse on the join without video button in the Zoom Client in under 7 seconds!!!!")
+time.sleep(7)
+x = pyg.position()[0]
+y = pyg.position()[1]
+print(f"Copy paste the follwing in place of x and y coordinates under the join_meeting_ID function in  the click on join without video button command x={x}, y={y}")
+print("The program will wait for you for 69 seconds!!!")
+time.sleep(69)
+      
+print("Position you mouse on the connect audio button in the Zoom Client in under 7 seconds!!!!")
+time.sleep(7)
+x = pyg.position()[0]
+y = pyg.position()[1]
+print(f"Copt paste the follwing in place of x and y coordinates under the join_meeting_ID function in  the click on conncet audio command x={x}, y={y}")
+print("The program will wait for you for 69 seconds!!!")
+time.sleep(69)
+
+print("Position you mouse on the mute button in the Zoom Client in under 7 seconds!!!!")
+time.sleep(7)
+x = pyg.position()[0]
+y = pyg.position()[1]
+print(f"Copt paste the follwing in place of x and y coordinates under the join_meeting_ID function in  the click on mute button command x={x}, y={y}")
+print("The program will wait for you for 69 seconds!!!")
+time.sleep(69)
+
